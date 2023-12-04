bff3df4e7bf64fcbaa1c552ba5aa17bff4633913
Richard Goulter <richard.goulter@gmail.com>
2023-12-04 14:32:26 +0700

    [Keymap] add rgoulter/pykey40/hsrgb vial

---
+++ keyboards/rgoulter/pykey40/hsrgb/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+// Copyright 2023 Richard Goulter
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE6, 0xE2, 0x1C, 0x03, 0x2E, 0x95, 0x00, 0x68}
+#define VIAL_UNLOCK_COMBO_ROWS {3, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 11}
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+#undef DYNAMIC_KEYMAP_LAYER_COUNT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
